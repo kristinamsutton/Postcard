21ddbb081c686f52d5b1870d67d6a2623a3faa61
Kristina Sutton <kristinamsutton@Kristina-Suttons-MacBook-Pro.local>
2015-02-11 17:08:43 -0600

    Initial Commit

---
+++ Post Card/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Post Card
+//
+//  Created by Kristina Sutton on 2/11/15.
+//  Copyright (c) 2015 Kristina Sutton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
